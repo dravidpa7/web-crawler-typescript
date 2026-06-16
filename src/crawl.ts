53f71e88c0929016f26fb3a0cb9a5c4a54f02d27
dravidpa7 <dravidpa20@gmail.com>
2026-06-16 18:02:34 +0000

    Unit test completed for base crawl

---
+++ src/crawl.ts
@@ -0,0 +1,98 @@
+import { JSDOM } from 'jsdom'
+
+export function normalizeURL(url: string){
+  const urlObj = new URL(url);
+  let fullPath = `${urlObj.host}${urlObj.pathname}`;
+  if (fullPath.slice(-1) === "/"){
+  fullPath = fullPath.slice(0,-1);
+  }
+  return fullPath; 
+}
+
+export function getHeadingFromHTML(html: string): string{
+   try {
+      const dom = new JSDOM(html);
+      const doc = dom.window.document
+      const h1 = doc.querySelector("h1") ?? doc.querySelector("h2")
+      return (h1?.textContent ?? "").trim()
+   } catch (error) {
+      return ""
+   }
+}
+
+export function getFirstParagraphFromHTML( html : string) :string{
+   try {
+      const dom = new JSDOM(html)
+      const doc = dom.window.document
+      const main = doc.querySelector('main')
+      const p = main?.querySelector('p')?? doc.querySelector('p');
+      return (p?.textContent ?? "").trim()
+   } catch {
+      return ""
+   }
+}
+
+export function getURLsFromHTML(html :string, baseURL :string) :string[]{
+   const urls: string[] = [];
+   try {
+      const dom = new JSDOM(html)
+      const doc = dom.window.document
+      const anchors = doc.querySelectorAll('a')
+      
+      anchors.forEach((anchor)=>{
+         const href = anchor.getAttribute('href');
+         if(!href) return ;
+         try{
+            const absoluteURL = new URL(href,baseURL).toString();
+            urls.push(absoluteURL);
+         }
+         catch(error){
+            console.log(`invalid href ${href}`, error)
+         }
+      })
+   } catch {
+      console.log("Failed to Parse HTML")
+   }
+   return urls;
+}
+
+export function getImagesFromHTML(html: string, baseURL: string) :string[]{
+   const imageURLs: string[] = [];
+   try {
+      const dom = new JSDOM(html)
+      const doc = dom.window.document
+      const images = doc.querySelectorAll('img')
+      images.forEach((img)=>{
+         const src = img.getAttribute('src');
+         if(!src) return ;
+         try {
+            const absoluteURL = new URL(src,baseURL).toString();
+            imageURLs.push(absoluteURL);
+         } catch (error) {
+            console.log(`invalid href ${src}`, error);
+         }
+      })
+   } catch (error) {
+      console.log(error);
+   }
+   return imageURLs;
+}
+
+export interface ExtractPageData{
+   url: string,
+   heading: string,
+   first_paragraph: string,
+   outgoing_links: string[],
+   image_urls: string[]
+}
+
+export function extractPageData(html : string, pageURL :string): ExtractPageData{
+
+   return{
+      url: pageURL,
+      heading: getHeadingFromHTML(html),
+      first_paragraph: getFirstParagraphFromHTML(html),
+      outgoing_links: getURLsFromHTML(html,pageURL),
+      image_urls: getImagesFromHTML(html,pageURL)
+   }
+}
