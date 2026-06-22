975dd8c6e64a9d39c1d597011b8592ef354ffa4d
dravidpa7 <dravidpa20@gmail.com>
2026-06-22 09:23:15 +0000

    feat : report added

---
+++ src/report.ts
@@ -0,0 +1,31 @@
+import { ExtractPageData } from "./crawl";
+import * as fs from 'fs';
+import * as path from 'path';
+
+
+export function writeJSONReport(
+    pageData : Record<string,ExtractPageData>,
+    fileName = "report.json",
+):void{
+    const sorted = Object.values(pageData).sort((a,b)=>a.url.localeCompare(b.url))
+    const data = JSON.stringify(sorted,null,2);
+
+    // const dirPath = path.join(process.cwd(), "reports");
+    // const filePath = path.join(dirPath, fileName);
+
+    // if (!fs.existsSync(dirPath)) {
+    //     fs.mkdirSync(dirPath,{recursive:true})
+    // }
+
+    const filePath = path.join(
+        process.cwd(),
+        "report.json"
+    );
+    
+    try {
+        fs.writeFileSync(filePath,data,'utf8');
+        console.log("File written on the report");
+    } catch (error) {
+        console.log(error)
+    }
+}
